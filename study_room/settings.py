e449eb01d5248b5db20c395d847bd37bc6da4fb0
Shawn <shawn@ShawndeMacBook-Pro.local>
2024-02-28 12:37:47 +0800

    fix:static-show

--- study_room/settings.py
+++ study_room/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,9 +132,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-  BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#   BASE_DIR / 'static'
+# ]
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
